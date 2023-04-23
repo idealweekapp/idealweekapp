d1fe8d84779f21f2e330e884a02ce097537dd86d
David Trelles <davidstrelles@gmail.com>
2023-04-23 15:22:08 -0500

    some parting words

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,38 +1,38 @@
-import React, { useEffect } from 'react';
+import React, { useEffect } from "react";
 import {
   HashRouter as Router,
   Redirect,
   Route,
   Switch,
-} from 'react-router-dom';
+} from "react-router-dom";
 
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch, useSelector } from "react-redux";
 
-import Nav from '../Nav/Nav';
-import Footer from '../Footer/Footer';
+import Nav from "../Nav/Nav";
+import Footer from "../Footer/Footer";
 
-import ProtectedRoute from '../ProtectedRoute/ProtectedRoute';
+import ProtectedRoute from "../ProtectedRoute/ProtectedRoute";
 
-import UserPage from '../UserPage/UserPage';
-import IntroPage from '../IntroPage/IntroPage';
-import LandingPage from '../LandingPage/LandingPage';
-import LoginPage from '../LoginPage/LoginPage';
-import RegisterPage from '../RegisterPage/RegisterPage';
-import WeekPage from '../WeekPage/WeekPage';
-import QuestionsPage from '../QuestionsPage/QuestionsPage';
-import PrioritiesPage from '../PrioritiesPage/PrioritiesPage';
-import AdminPage from '../AdminPage/AdminPage';
-import './App.css';
-
+import UserPage from "../UserPage/UserPage";
+import IntroPage from "../IntroPage/IntroPage";
+import LandingPage from "../LandingPage/LandingPage";
+import LoginPage from "../LoginPage/LoginPage";
+import RegisterPage from "../RegisterPage/RegisterPage";
+import WeekPage from "../WeekPage/WeekPage";
+import QuestionsPage from "../QuestionsPage/QuestionsPage";
+import PrioritiesPage from "../PrioritiesPage/PrioritiesPage";
+import AdminPage from "../AdminPage/AdminPage";
+import EditQuestionsPage from "../EditQuestionsPage/EditQuestionsPage";
+import "./App.css";
 
 function App() {
   const dispatch = useDispatch();
 
-  const user = useSelector(store => store.user);
+  const user = useSelector((store) => store.user);
 
   useEffect(() => {
-    dispatch({ type: 'FETCH_USER' });
-    dispatch({ type: 'FETCH_CATEGORIES' });
+    dispatch({ type: "FETCH_USER" });
+    dispatch({ type: "FETCH_CATEGORIES" });
   }, [dispatch]);
 
   return (
@@ -50,19 +50,23 @@ function App() {
 
           <ProtectedRoute exact path="/user">
             {/* logged in shows UserPage (OR IntroPage), else shows LoginPage */}
-            {!user.introComplete ?
-              <Redirect to="/intro" />  // If introComplete is falsy, redirect to IntroPage
-              :
-              <UserPage />  // else, go to userPage
+            {
+              !user.introComplete ? (
+                <Redirect to="/intro" /> // If introComplete is falsy, redirect to IntroPage
+              ) : (
+                <UserPage />
+              ) // else, go to userPage
             }
           </ProtectedRoute>
 
           <ProtectedRoute exact path="/intro">
             {/* logged in shows IntroPage (OR UserPage), else shows LoginPage */}
-            {user.introComplete ?
-              <Redirect to="/user" />  // If introComplete is truthy, redirect to UserPage
-              :
-              <IntroPage />  // else, go to IntroPage
+            {
+              user.introComplete ? (
+                <Redirect to="/user" /> // If introComplete is truthy, redirect to UserPage
+              ) : (
+                <IntroPage />
+              ) // else, go to IntroPage
             }
           </ProtectedRoute>
 
@@ -82,31 +86,52 @@ function App() {
           </ProtectedRoute>
 
           <ProtectedRoute exact path="/admin">
-            {/* logged in shows AdminPage else shows LoginPage */}
-            <AdminPage />
+            {
+              !user.admin ? (
+                <Redirect to="/user" /> // If the user is not admin, redirect to /user
+              ) : (
+                <AdminPage />
+              ) // Otherwise, show the admin page
+            }
+          </ProtectedRoute>
+
+          <ProtectedRoute exact path="/editquestions">
+            {
+              !user.admin ? (
+                <Redirect to="/user" /> // If the user is not admin, redirect to /user
+              ) : (
+                <EditQuestionsPage />
+              ) // Otherwise, show the edit questions page
+            }
           </ProtectedRoute>
 
           <Route exact path="/login">
-            {user.id ?
-              <Redirect to="/user" />  // If the user is already logged in, redirect to /user
-              :
-              <LoginPage />  // Otherwise, show the login page
+            {
+              user.id ? (
+                <Redirect to="/user" /> // If the user is already logged in, redirect to /user
+              ) : (
+                <LoginPage />
+              ) // Otherwise, show the login page
             }
           </Route>
 
           <Route exact path="/registration">
-            {user.id ?
-              <Redirect to="/user" />  // If the user is already logged in, redirect to /user
-              :
-              <RegisterPage />  // Otherwise, show the registration page
+            {
+              user.id ? (
+                <Redirect to="/user" /> // If the user is already logged in, redirect to /user
+              ) : (
+                <RegisterPage />
+              ) // Otherwise, show the registration page
             }
           </Route>
 
           <Route exact path="/home">
-            {user.id ?
-              <Redirect to="/user" />  // If the user is already logged in, redirect to /user
-              :
-              <LandingPage />  // Otherwise, show the Landing page
+            {
+              user.id ? (
+                <Redirect to="/user" /> // If the user is already logged in, redirect to /user
+              ) : (
+                <LandingPage />
+              ) // Otherwise, show the Landing page
             }
           </Route>
 
@@ -114,7 +139,6 @@ function App() {
           <Route>
             <h1>404</h1>
           </Route>
-
         </Switch>
         <Footer />
       </div>
